b15e4d41ed8d93b5b5d7a2483d9fd06d739dd28f
Caitlin Cabrera <caitlin.cabrera01@gmail.com>
2020-04-21 18:17:17 -0400

    First commit

--- app.js
+++ app.js
@@ -2,56 +2,94 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-//const { Pool } = require('mysql').Pool
 let mysql = require('mysql');
+let populations;
+const router = express.Router();
 
-
-//const pool = new Pool({
 let connection = mysql.createConnection({
   user: 'caitlin',
   host: 'localhost',
   database: '601_project',
-  password: '',
+  password: 'm********',
 });
-
 connection.connect(function(err) {
   if (err) {
     return console.error('error: ' + err.message);
   }
 
 });
+// do not remove below
+let createTodos = "SELECT * FROM irsincomebyzipcode;"
 
-  let createTodos = "SELECT * FROM irsincomebyzipcode;"
-
-  //app.get('/index.html', function(req,res) {
   connection.query(createTodos, function(err, results, fields) {
-    console.log(results);
-    //added next line
-  //  results.render('index.html', {data:results, error:null});
    // console.log(results);
+    //('#test').html(JSON.stringify(results));
+
     if (err) {
       console.log(err.message);
     }
   });
+/*  connection.end(function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
 
+  });*/
+
+// do not remove above
+
+app.get("/",(req, results) => {
+  results.sendFile(__dirname + "/index.html");
+  console.log('Accessing the secret section ...')
+});
+
+//routes
+
+app.get('/lowincomebyzip', (req, res) =>{
+  let dataSent = [];
+  connection.query("SELECT hospitalGeneralInformation.facility_name, hospitalGeneralInformation.city, hospitalGeneralInformation.state, address, hospital_overall_rating \n" +
+      "FROM hospitalGeneralInformation JOIN irsincomebyzipcode\n" +
+      "WHERE hospital_overall_rating <= 3 AND avg_agi <= 67565 AND hospitalGeneralInformation.zipcode = irsincomebyzipcode.zipcode\n" +
+      "ORDER BY hospital_overall_rating;  " ,(error, results) => {
+    if (error) {
+      throw error
+    }
+    dataSent.push(results);
+    res.json(dataSent);
+   console.log(results);
+  })
+
+
+});
+
+app.get('/highincomebyzip', (req, res) =>{
+  let dataSent2 = [];
+  connection.query("SELECT hospitalGeneralInformation.facility_name, hospitalGeneralInformation.city, hospitalGeneralInformation.state, address, hospital_overall_rating \n" +
+      "FROM hospitalGeneralInformation JOIN irsincomebyzipcode\n" +
+      "WHERE hospital_overall_rating > 3 AND avg_agi < 67565 AND hospitalGeneralInformation.zipcode = irsincomebyzipcode.zipcode\n" +
+      "ORDER BY hospital_overall_rating;  " ,(error, results) => {
+    if (error) {
+      throw error
+    }
+    dataSent2.push(results);
+    res.json(dataSent2);
+    console.log(results);
+  })
   connection.end(function(err) {
     if (err) {
       return console.log(err.message);
     }
 
-   // console.log(results);
   });
-//});
+
+})
 
 
 
-
-
-//==========================================================================
 app.use(express.static(__dirname + '/HTML'));
 app.use(express.static(__dirname + '/js'));
 app.use(express.static(__dirname + '/CSS'));
 app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/index.html'));
 
-
-app.listen(8080, () => {console.log("started on port 8080");});
+app.listen(8080, () => {console.log(" Caitlin's 601 project is running on port 8080");});
